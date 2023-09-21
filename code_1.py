0a682ea139ea4879b15722cf68e6b31ee7aab641
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-09-21 15:53:08 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,7 +1,7 @@
 
 def generate_random_data():
-    random_string = 'Prepare shoulder miss family produce budget.'
-    random_number = 58
+    random_string = 'Role range loss yard instead.'
+    random_number = 47
 
     data = [(random_string, random_number) for _ in range(10)]
     return data
@@ -10,8 +10,8 @@ def main():
     data = generate_random_data()
     for item in data:
         random_string, random_number = item
-        print(f"Random String: Prepare shoulder miss family produce budget.")
-        print(f"Random Number: 58")
+        print(f"Random String: Role range loss yard instead.")
+        print(f"Random Number: 47")
 
 if __name__ == "__main__":
     main()
